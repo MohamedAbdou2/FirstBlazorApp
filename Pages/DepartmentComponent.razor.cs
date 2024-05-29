d74d8a82fcdbe09224ea88fedd4b36edc9326ce0
mohamed abdou <modohoda2468@gmail.com>
2024-05-29 20:15:50 +0300

    Add project files.

---
+++ Pages/DepartmentComponent.razor.cs
@@ -0,0 +1,54 @@
+﻿using FirstBlazorApp.Models;
+
+namespace FirstBlazorApp.Pages
+{
+    public partial class DepartmentComponent
+    {
+        public int SelectDeptId { get; set; } = 3;
+        public List<Department> DepartmentList { get; set; }
+        public Department Department { get; set; }
+
+        public DepartmentComponent()
+        {
+            DepartmentList = new List<Department>();
+            DepartmentList.Add(new Department()
+            {
+                Id = 1,
+                Name = "SD",
+                ManagerImage = "manager.jpg"
+
+            });
+
+            DepartmentList.Add(new Department()
+            {
+                Id = 2,
+                Name = "OS",
+                ManagerImage = "woman.jpeg"
+
+            });
+
+            DepartmentList.Add(new Department()
+            {
+                Id = 3,
+                Name = "PD",
+                ManagerImage = "manager.jpg"
+
+            });
+
+            Department = new Department()
+            {
+                Id = 1,
+                Name = "SD",
+                ManagerImage = "manager.jpg"
+
+            };
+
+
+        }
+
+        private void LoadDept()
+        {
+            Department = DepartmentList.FirstOrDefault(d => d.Id == SelectDeptId);
+        }
+    }
+}
---
+++ Pages/ProductComponent.razor.cs
@@ -0,0 +1,59 @@
+﻿using FirstBlazorApp.Models;
+
+namespace FirstBlazorApp.Pages
+{
+    public partial class ProductComponent
+    {
+        public int SelectId { get; set; } = 1;
+
+        public List<Product> ProductList { get; set; }
+
+        public Product Product { get; set; }
+
+        public ProductComponent()
+        {
+
+            ProductList = new List<Product>();
+            ProductList.Add(new Product()
+            {
+                Id = 1,
+                Name = "Apple",
+                Image = "iphone.jpeg",
+                Price = 10000
+            });
+            ProductList.Add(new Product()
+            {
+                Id = 2,
+                Name = "Realme",
+                Image = "realme.jpeg",
+                Price = 11000
+            });
+
+            ProductList.Add(new Product()
+            {
+                Id = 3,
+                Name = "Oppo",
+                Image = "oppp.jpeg",
+                Price = 12000
+            });
+
+            Product = new Product()
+            {
+                Id = 3,
+                Name = "Oppo",
+                Image = "oppp.jpeg",
+                Price = 12000
+
+            };
+
+
+        }
+
+        private void LoadProduct()
+        {
+            Product = ProductList.FirstOrDefault(p => p.Id == SelectId);
+        }
+
+
+    }
+}
